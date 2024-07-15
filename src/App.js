fe6384cee0208c207173168ca09c73596494649e
vershat <96232099+vershat@users.noreply.github.com>
2024-07-15 14:36:34 +0530

    Versha Saved-Post  commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import SavePos from './components/SavePos';
 import Quora from './components/Quora';
 //import { Leaderboard } from '@mui/icons-material';
 import Leaderboard from './components/LeaderBoardComp';
 function App() {
   return (
     <div className="App">
-     
+      
     <Header></Header>
-   <Quora></Quora> 
-   {/* <Leaderboard></Leaderboard> */}
+   <Quora></Quora>  
+    {/* <Leaderboard></Leaderboard> 
+   <SavePos></SavePos> */}
     </div>
   );
 }

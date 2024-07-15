95c66dc7ef0d28d96d76a73d8024656f8aa9b134
vershat <96232099+vershat@users.noreply.github.com>
2024-07-15 13:27:26 +0530

    Versha LeaderBoard  commit

--- src/App.js
+++ src/App.js
@@ -14,3 +14,37 @@ function App() {
 }
 
 export default App;
+
+
+// export default App;
+// import React from 'react';
+// import './App.css';
+// import Header from './components/Header';
+// import QHeader from './components/QHeader';
+// import Sidebar from './components/Sidebar';
+// import Widget from './components/Widget';
+// import Leaderboard from './components/LeaderBoardComp';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <QHeader />
+//       <div className="main-content">
+//         <Sidebar className="sidebar" />
+//         <div className="leaderboard-container">
+//           <Leaderboard className="leaderboard" />
+//           <div className="widget-container">
+//             <Widget className="widget" />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
